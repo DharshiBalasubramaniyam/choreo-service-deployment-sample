26cdfe2eae4648e1d6e2f61d42ef36d9ac74281c
dharshi <dharshib.8@gmail.com>
2025-03-28 09:39:13 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// Enable CORS
+app.use(cors());
+
+// Test GET endpoint
+app.get('/test', (req, res) => {
+    res.json({ message: 'Hello, world!' });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
